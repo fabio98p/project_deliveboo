9079ef644cdf96eb11b708cf6997a77c3fc8a78d
emanuele <emattina97@gmail.com>
2021-06-18 12:12:19 +0200

    Nomi categorie corretti + percorso image

--- config/categories.php
+++ config/categories.php
@@ -2,10 +2,54 @@
 return [
     [
         'name' => 'italiano',
-        'icon' => '',
+        'icon' => '/images/categories/italiano.png',
     ],
     [
-        'name' => 'italiano',
-        'icon' => '',
+        'name' => 'messicano',
+        'icon' => '/images/categories/messicano.png',
+    ],
+    [
+        'name' => 'cinese',
+        'icon' => '/images/categories/cinese.png',
+    ],
+    [
+        'name' => 'americano',
+        'icon' => '/images/categories/americano.png',
+    ],
+    [
+        'name' => 'indiano',
+        'icon' => '/images/categories/indiano.png',
+    ],
+    [
+        'name' => 'giapponese',
+        'icon' => '/images/categories/giapponese.png',
+    ],
+    [
+        'name' => 'gelateria',
+        'icon' => '/images/categories/gelateria.png',
+    ],
+    [
+        'name' => 'kebab',
+        'icon' => '/images/categories/kebab.png',
+    ],
+    [
+        'name' => 'vegetariano',
+        'icon' => '/images/categories/vegetariano.png',
+    ],
+    [
+        'name' => 'fastfood',
+        'icon' => '/images/categories/fastfood.png',
+    ],
+    [
+        'name' => 'pizzeria',
+        'icon' => '/images/categories/pizzeria.png',
+    ],
+    [
+        'name' => 'dessert',
+        'icon' => '/images/categories/dessert.png',
+    ],
+    [
+        'name' => 'poke',
+        'icon' => '/images/categories/poke.png',
     ],
 ];
