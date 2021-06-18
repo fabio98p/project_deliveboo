978bb73fed217ccaa8cdeb9e3426f7f7a8e6a80d
emanuele <emattina97@gmail.com>
2021-06-18 12:08:28 +0200

    Carico immagini categories

--- config/categories.php
+++ config/categories.php
@@ -1,16 +1,11 @@
 <?php
 return [
-    ['name' => 'italiano'],
-    ['name' => 'messicano'],
-    ['name' => 'cinese'],
-    ['name' => 'americano'],
-    ['name' => 'indiano'],
-    ['name' => 'giapponese'],
-    ['name' => 'gelateria'],
-    ['name' => 'kebab'],
-    ['name' => 'vegetariano'],
-    ['name' => 'fast-food'],
-    ['name' => 'pizzeria'],
-    ['name' => 'dessert'],
-    ['name' => 'poke'],
+    [
+        'name' => 'italiano',
+        'icon' => '',
+    ],
+    [
+        'name' => 'italiano',
+        'icon' => '',
+    ],
 ];
