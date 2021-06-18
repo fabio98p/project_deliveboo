547b018285701be1498877d69e6b4111125c0b40
fabio <parisifabio98@gmail.com>
2021-06-18 11:08:07 +0200

    aggiunto altri piatti

--- config/dishes.php
+++ config/dishes.php
@@ -78,9 +78,90 @@ return [
     [
         "name" => "pasta con le vongole",
         "description" => "la pasta per chi non si accontenta di mangiare ma vuole anche la sfida di aprire quelle cazzo di vongole",
-        "price" => 5.99,
+        "price" => 10.99,
         "available" => "true",
         "image" => "",
     ],
     //tacos
+    [
+        "name" => "Tacos classico",
+        "description" => "Tacos classico, con semplicemente tutti gli ingredienti, non e' cosi un tacos classico??",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "Tacos piccante",
+        "description" => "Tacos classico ma con molto piu piccante",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "Tacos vegetariano",
+        "description" => "Tacos classico ma sena carne, tranquilli pero, il picvcante c'e' comunque",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    //burrito
+    [
+        "name" => "Burrito classico",
+        "description" => "Burrito classico, con semplicemente tutti gli ingredienti, non e' cosi un burrito classico??",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "Burrito piccante",
+        "description" => "Burrito classico ma con molto piu piccante",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "Burrito vegetariano",
+        "description" => "Burrito classico ma sena carne, tranquilli pero, il picvcante c'e' comunque",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    //cinese
+    [
+        "name" => "Riso alla cantonese",
+        "description" => "Riso tipico cinese con prosciutto cotto",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "anatra all'arancia",
+        "description" => "anatra all'arancia, non chiedetemi il perche dell'arancia",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "involtini di gamberi",
+        "description" => "semplici involtini di gamberi",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    [
+        "name" => "tofu con carne piccante",
+        "description" => "tofu con carne piccante",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+    //amerticano
+    [
+        "name" => "Hamburger generico",
+        "description" => "hamburger generico con 2000kcal",
+        "price" => 5.99,
+        "available" => "true",
+        "image" => "",
+    ],
+
 ];
