2bbb54c7841f02ce34835ac2fd0ecd3491dfa412
fabio <parisifabio98@gmail.com>
2021-06-20 17:22:40 +0200

    seeder category

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use Config\categories;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = config('categories');
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->name = $category['name'];
+            $newCategory->icon = $category['icon'];
+            $newCategory->slug = // creare funzione slug
+            $newCategory->save();
+        }
     }
 }
