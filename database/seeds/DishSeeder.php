468a94e0052e1debdd155c0b89aae4932e07211e
Dora Budić <dbudic0706@gmail.com>
2021-06-21 14:48:53 +0200

    Seeder modified

--- database/seeds/DishSeeder.php
+++ database/seeds/DishSeeder.php
@@ -20,10 +20,26 @@ class DishSeeder extends Seeder
             $newDish->description = $dish['description'];
             $newDish->price = $dish['price'];
             $newDish->available = $dish['available'];
-            // $newDish->image = $dish['image'];
-            $newDish->slug = Str::slug($dish['name'],'-');
+            $newDish->image = $dish['image'];
+            $newDish->slug = $this->generateSlug($dish['name']);
             $newDish->restaurant_id = $dish['restaurant_id'];
             $newDish->save();
         }
+    }
+
+    private function generateSlug(string $title) {
+
+      $slug = Str::slug($title,'-');
+      $slug_base = $slug;
+      $contatore = 1;
+
+      $post_with_slug = Dish::where('slug','=',$slug)->first();
+      while($post_with_slug) {
+        $slug = $slug_base . '-' . $contatore;
+        $contatore++;
+
+        $post_with_slug = Dish::where('slug','=',$slug)->first();
+      }
+      return $slug;
     }
 }
--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -22,9 +22,25 @@ class RestaurantSeeder extends Seeder
             $newRestaurant->description = $restaurant['description'];
             $newRestaurant->banner = $restaurant['banner'];
             $newRestaurant->available = $restaurant['available'];
-            $newRestaurant->slug = Str::slug($restaurant['name'],'-');
+            $newRestaurant->slug = $this->generateSlug($restaurant['name']);
             $newRestaurant->user_id = rand(1,5);
             $newRestaurant->save();
         }
+    }
+
+    private function generateSlug(string $title) {
+
+      $slug = Str::slug($title,'-');
+      $slug_base = $slug;
+      $contatore = 1;
+
+      $post_with_slug = Restaurant::where('slug','=',$slug)->first();
+      while($post_with_slug) {
+        $slug = $slug_base . '-' . $contatore;
+        $contatore++;
+
+        $post_with_slug = Restaurant::where('slug','=',$slug)->first();
+      }
+      return $slug;
     }
 }
