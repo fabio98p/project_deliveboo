f814b6984f2e4268b706b8b76aee129bd9e76cbe
fabio <parisifabio98@gmail.com>
2021-06-22 18:11:28 +0200

    sistemato migration category restaurant (api funzionanti)

--- database/migrations/2021_06_18_145406_create_category_restaurant_table.php
+++ database/migrations/2021_06_18_145406_create_category_restaurant_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRestaurantCategoryTable extends Migration
+class CreateCategoryRestaurantTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRestaurantCategoryTable extends Migration
      */
     public function up()
     {
-      Schema::create('restaurant_category', function (Blueprint $table) {
+      Schema::create('category_restaurant', function (Blueprint $table) {
         $table->unsignedBigInteger('category_id');
         $table->foreign('category_id')
               ->references('id')
@@ -35,6 +35,6 @@ class CreateRestaurantCategoryTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('restaurant_category');
+        Schema::dropIfExists('category_restaurant');
     }
 }
