48db3c2606615d8d9207cfee55c103de83e21225
emanuele <emattina97@gmail.com>
2021-06-23 15:49:02 +0200

    Migliorie grafiche viste admin

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,156 +1,149 @@
 <!-- Footer -->
 <footer class=" text-center color-darkwhite">
-    <!-- Grid container -->
-    <div class="container p-4">
-      <!-- Section: Social media -->
+<!-- Grid container -->
+<div class="container p-4">
+<!-- Section: Social media -->
 
-      <!-- Section: Social media -->
+<!-- Section: Social media -->
 
-      <!-- Section: Form -->
-      <section class="">
-        <form action="">
-          <!--Grid row-->
-          <div class="row d-flex justify-content-center">
-            <!--Grid column-->
-            <div class="col-auto">
-              <p class="pt-2">
-                <strong>Hai qualche Problema? Contattaci qui</strong>
-              </p>
-            </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-md-5 col-12">
-              <!-- Email input -->
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="form5Example2" class="form-control" />
-                <label class="form-label" for="form5Example2">Your Email address</label>
-              </div>
-            </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-auto">
-              <!-- Submit button -->
-              <button type="submit" class="btn btn-outline-light mb-4">
-                Compila Form
-              </button>
-            </div>
-            <!--Grid column-->
-          </div>
-          <!--Grid row-->
-        </form>
-      </section>
-      <!-- Section: Form -->
-
-      <!-- Section: Text -->
-      <section class="mb-4">
-
-      </section>
-      <!-- Section: Text -->
-
-      <!-- Section: Links -->
-      <section class="links">
-        <!--Grid row-->
-        <div class="row">
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
-            <h5 class="text-uppercase color-orange font-weight-bold">Scopri Deliveboo</h5>
-
-            <ul class="list-unstyled mb-0 ">
-              <li>
-                <a href="#!" class="color-darkwhite">Programmazione</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Design</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Contatti</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Collabora con noi</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
-            <h5 class="text-uppercase color-orange font-weight-bold">Creators</h5>
-
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="color-darkwhite">Attinà Emanuele</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Budic Dora</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Parisi Fabio</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Pignattari giorgio</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Tabbì Filippo</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
-            <h5 class="text-uppercase color-orange font-weight-bold">Note legali</h5>
-
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="color-darkwhite">Termini & Condizioni</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Informativa sulla privacy</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Cookies</a>
-              </li>
-              <li>
-
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
-            <h5 class="text-uppercase color-orange font-weight-bold">Aiuto</h5>
-
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="color-darkwhite">Contatti</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">FAQ</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Tipi di cucina</a>
-              </li>
-              <li>
-                <a href="#!" class="color-darkwhite">Mappa del sito</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
+<!-- Section: Form -->
+<section class="">
+    <form action="">
+    <!--Grid row-->
+    <div class="row d-flex justify-content-center">
+        <!--Grid column-->
+        <div class="col-auto">
+            <p class="pt-2">
+                <strong>Hai qualche Problema?</strong>
+            </p>
         </div>
-        <!--Grid row-->
-      </section>
-      <!-- Section: Links -->
-    </div>
-    <!-- Grid container -->
+        <!--Grid column-->
 
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2020 Copyright:
-      <a class="color-darkwhite" href="https://mdbootstrap.com/">Deliveboo.com</a>
+        <!--Grid column-->
+        <div class="col-md-5 col-12">
+            <!-- Email input -->
+            <div class="form-outline form-white mb-4">
+                <input type="email" id="form5Example2" class="form-control" placeholder="Contattaci qui" />
+            </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-light mb-4">
+                Compila Form
+            </button>
+        </div>
+        <!--Grid column-->
     </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+    <!--Grid row-->
+    </form>
+</section>
+<!-- Section: Form -->
+
+<!-- Section: Links -->
+<section class="links">
+    <!--Grid row-->
+    <div class="row">
+    <!--Grid column-->
+    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
+        <h5 class="text-uppercase color-orange font-weight-bold">Scopri Deliveboo</h5>
+
+        <ul class="list-unstyled mb-0 ">
+            <li>
+                <a href="#!" class="color-darkwhite">Programmazione</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Design</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Contatti</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Collabora con noi</a>
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
+        <h5 class="text-uppercase color-orange font-weight-bold">Creators</h5>
+
+        <ul class="list-unstyled mb-0">
+            <li>
+                <a href="#!" class="color-darkwhite">Attinà Emanuele</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Budic Dora</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Parisi Fabio</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Pignattari giorgio</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Tabbì Filippo</a>
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
+        <h5 class="text-uppercase color-orange font-weight-bold">Note legali</h5>
+
+        <ul class="list-unstyled mb-0">
+            <li>
+                <a href="#!" class="color-darkwhite">Termini & Condizioni</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Informativa sulla privacy</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Cookies</a>
+            </li>
+            <li>
+
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-left">
+        <h5 class="text-uppercase color-orange font-weight-bold">Aiuto</h5>
+
+        <ul class="list-unstyled mb-0">
+            <li>
+                <a href="#!" class="color-darkwhite">Contatti</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">FAQ</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Tipi di cucina</a>
+            </li>
+            <li>
+                <a href="#!" class="color-darkwhite">Mappa del sito</a>
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+    </div>
+    <!--Grid row-->
+</section>
+<!-- Section: Links -->
+</div>
+<!-- Grid container -->
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+© 2020 Copyright:
+<a class="color-darkwhite" href="https://mdbootstrap.com/">Deliveboo.com</a>
+</div>
+<!-- Copyright -->
+</footer>
+<!-- Footer -->
