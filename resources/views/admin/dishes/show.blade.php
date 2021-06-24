4d6d707035a24a5b98264ce77d0eddddac01b503
emanuele <emattina97@gmail.com>
2021-06-24 22:07:00 +0200

    Asset in url immagini

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -21,7 +21,7 @@
 
     <div class="row">
         <div class="col-md-4">
-            <div class="dish-show" style="background-image: url('{{$dish['image']}}')">
+            <div class="dish-show" style="background-image: url('{{asset($dish->image)}}')">
 
             </div>
         </div>
