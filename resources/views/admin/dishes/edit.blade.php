c3e76da0f1f0959fbb50ab376f12c137dbda60ab
emanuele <emattina97@gmail.com>
2021-06-24 16:04:11 +0200

    Id ristorante edit dish

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -13,6 +13,20 @@
                         @method('PUT')
 
                         <div class="form-group row">
+                            <label for="id" class="col-md-4 col-form-label text-md-right">Id Ristorante</label>
+
+                            <div class="col-md-6">
+                                <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ $restaurant->id }}" required autocomplete="id" autofocus disabled>
+
+                                @error('id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
