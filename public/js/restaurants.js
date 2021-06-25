90083020c402cd43f7feed142743d32da9926572
Filippo <filippotabbi23@gmail.com>
2021-06-25 17:41:48 +0200

    homepage con api categorie

--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -97,12 +97,19 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
+    scriviTxt: '',
     restaurants: [],
+    categories: [],
+    searchResultRestaurant: [],
+    searchResultDish: [],
     checkClick: false
   },
   created: function created() {
     var _this = this;
 
+    axios.get('http://localhost:8000/api/categories').then(function (response) {
+      _this.categories = response.data.response;
+    });
     axios.get('http://localhost:8000/api/restaurants').then(function (response) {
       _this.restaurants = response.data.response;
     });
@@ -110,6 +117,16 @@ var app = new Vue({
   methods: {
     checkReverse: function checkReverse() {
       this.checkClick = !this.checkClick;
+    },
+    cerca: function cerca(scriviTxt) {
+      var _this2 = this;
+
+      axios.get("http://localhost:8000/api/restaurants?name=" + scriviTxt).then(function (response) {
+        _this2.searchResultRestaurant = response.data.results;
+      });
+      axios.get("http://localhost:8000/api/restaurant?name=" + scriviTxt).then(function (response) {
+        _this2.searchResultDish = response.data.results;
+      });
     }
   }
 });
@@ -123,7 +140,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/emanueleattina/Documents/progetti_fallimentari/boolean/Esercizi/project_deliveboo/resources/js/restaurants.js */"./resources/js/restaurants.js");
+module.exports = __webpack_require__(/*! C:\Users\filippo\Desktop\boolean\Esercizi\php\project_deliveboo\resources\js\restaurants.js */"./resources/js/restaurants.js");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+
 </head>
 
 <body>
@@ -33,6 +33,7 @@
         @yield('main')
     </main>
     @include('partials.footer')
+
 
     {{-- cdn e script --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
