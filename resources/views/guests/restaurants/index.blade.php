9c89a3a4ed560bfdd0b8d1366c46d083e371394c
Giorgio <giorgio.pignattari@gmail.it>
2021-06-25 18:06:29 +0200

    creata show blade di guests

--- resources/views/guests/restaurants/index.blade.php
+++ resources/views/guests/restaurants/index.blade.php
@@ -3,28 +3,24 @@
 @section('main')
 <main id="root">
   <div class="container-fluid banner-show" style="background-image: url('../images/varie/my-restaurant-banner.jpg')"></div>
-  <section class="section-main">
+    <section class="section-main">
       <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-12">
-                <div class="row">
-                 <div class="consigliati flex-wrap">
+        <div class="row justify-content-center">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="consigliati flex-wrap">
 
-                          <div class="card-homepage" v-for="restaurant in restaurants" :key="restaurants.id ">
-                            <h4>@{{restaurant.name}}</h4>
-                            <p>@{{restaurant.address}}</p>
-                            <p>@{{restaurant.description}}</p>
-                          </div>
-
+                  <div class="card-homepage" v-for="restaurant in restaurants" :key="restaurants.id ">
+                    <h4>@{{restaurant.name}}</h4>
+                    <p>@{{restaurant.address}}</p>
+                    <p>@{{restaurant.description}}</p>
                   </div>
-                </div>
 
               </div>
+            </div>
           </div>
-
+        </div>
       </div>
   </section>
-
 </main>
-
 @endsection
