6b6d1d4f87c138d198b5ba472088ec21d759f0a9
emanuele <emattina97@gmail.com>
2021-06-27 14:56:44 +0200

    Modifiche form

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -2,129 +2,111 @@
 
 @section('main')
 <section class="section-main">
-  <div class="container" id="root">
-      <div class="row justify-content-center">
-          <div class="col-md-8">
-              <div class="card">
-                  <div class="card-header align-center">CREA RISTORANTE</div>
+<div class="container" id="root">
+    <div class="row justify-content-center">
+        <div class="col-md-8 offset-md-4">
+            <div class="">
+                <div class="col-md-12">
+                    <div class="page-top">
+                        <h1>Crea ristorante</h1>
+                    </div>
+                </div>
+                
 
-                  <div class="card-body">
-                      <form method="POST" action="{{ route('admin.restaurants.store') }}" enctype="multipart/form-data">
-                          @csrf
-                          @method('POST')
+                    <form method="POST" action="{{ route('admin.restaurants.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('POST')
 
-                          <div class="form-group row">
-                              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="form-group column">
+                            <label for="name" class="col-md-4 col-form-label label-personal">{{ __('Nome') }}</label>
 
-                              <div class="col-md-6">
-                                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                  @error('name')
-                                      <small class="text-danger">{{ $message }}</small>
-                                  @enderror
-                              </div>
-                          </div>
+                                @error('name')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                        </div>
 
-                          <div class="form-group row">
-                              <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+                        <div class="form-group column">
+                            <label for="address" class="col-md-4 col-form-label label-personal">{{ __('Indirizzo') }}</label>
 
-                              <div class="col-md-6">
-                                  <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
 
-                                  @error('address')
-                                      <small class="text-danger">{{ $message }}</small>
-                                  @enderror
-                              </div>
-                          </div>
+                                @error('address')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                        </div>
 
-                          <div class="form-group row">
-                              <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+                        <div class="form-group column">
+                            <label for="description" class="col-md-4 col-form-label label-personal">{{ __('Descrizione') }}</label>
 
-                              <div class="col-md-6">
-                                  <textarea id="description" type="description" class="form-control @error('description') is-invalid @enderror" name="description" required autocomplete="description">{{ old('description') }}</textarea>
+                            <div class="col-md-6">
+                                <textarea id="description" type="description" class="form-control @error('description') is-invalid @enderror" name="description" required autocomplete="description">{{ old('description') }}</textarea>
 
-                                  @error('description')
-                                      <small class="text-danger">{{ $message }}</small>
-                                  @enderror
-                              </div>
-                          </div>
+                                @error('description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                        </div>
 
-                          {{-- <div class="form-group row">
-                            <label for="category" class="col-md-4 col-form-label text-md-right">{{ __('Category') }}</label>
-                              <select class="form-control @error('category_ids') is-invalid @enderror" name="category_ids[]" multiple>
-                                  @foreach ($categories as $index => $category)
-                                      <option value="{{$category->id}}">{{$category->name}}</option>
-                                  @endforeach
-                              </select>
-                              @error('category_ids')
-                                  <small class="text-danger">{{ $message }}</small>
-                              @enderror
-                          </div> --}}
+                        <div class="form-group column pos-rel">
+                            <label for="description" class="col-md-4 col-form-label label-personal">{{ __('Categoria') }}</label>
+                            <div class="col-md-6">
+                                <div class="selectBox">
+                                    <select>
+                                        <option>Scegli</option>
+                                    </select>
+                                    <div class="overSelect" v-on:click="checkReverse()"></div>
+                                </div>
+                                <div :class="(checkClick == true) ? 'show-this' : 'hide-this' ">
+                                    @foreach ($categories as $index => $category)
+                                        <label>
+                                            <input type="checkbox" name="category_ids[]" value="{{$category->id}}"/>{{$category->name}}</label>
+                                    @endforeach
+                                </div>
+                            </div>
+                            
+                        </div>
 
-                          <div class="form-group column pos-rel">
-                              <div class="selectBox">
-                                  <select>
-                                      <option>Categoria</option>
-                                  </select>
-                                  <div class="overSelect" v-on:click="checkReverse()"></div>
-                              </div>
-                              <div :class="(checkClick == true) ? 'show-this' : 'hide-this' ">
-                                  @foreach ($categories as $index => $category)
-                                      <label>
-                                          <input type="checkbox" name="category_ids[]" value="{{$category->id}}"/>{{$category->name}}</label>
-                                  @endforeach
-                              </div>
-                          </div>
+                        <!-- upload logo -->
+                        <div class="form-group column">
+                            <div class="col-md-6">
+                                <label for="logo" class="label-personal">Logo</label>
+                                <input class="form-control-file @error('logo') is-invalid @enderror" id="logo" name="logo" type="file">
+                                @error('logo')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                        </div>
 
-                          <!-- upload logo -->
-                          <div class="form-group row">
-                              <label for="logo">Logo</label>
-                              <input class="form-control-file @error('logo') is-invalid @enderror" id="logo" name="logo" type="file">
-                              @error('logo')
-                                  <small class="text-danger">{{ $message }}</small>
-                              @enderror
-                          </div>
-                          <!-- upload logo -->
-                          <!-- upload file banner -->
-                          <div class="form-group row">
-                              <label for="banner">Banner</label>
-                              <input class="form-control-file @error('banner') is-invalid @enderror" id="banner" name="banner" type="file">
-                              @error('banner')
-                                  <small class="text-danger">{{ $message }}</small>
-                              @enderror
-                          </div>
-                            <!-- upload file banner -->
+                        
+                        <!-- upload logo -->
+                        <!-- upload file banner -->
+                        <div class="form-group column">
+                            <div class="col-md-6">
+                                <label for="banner" class="label-personal">Banner</label>
+                                <input class="form-control-file @error('banner') is-invalid @enderror" id="banner" name="banner" type="file">
+                                @error('banner')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                        </div>
 
-                          <!-- disponibilità si no -->
-                          {{-- <label for="banner">Disponibile:</label>
-                          <div class="form-check form-check-inline">
-                              <input class="form-check-input form-control @error('available') is-invalid @enderror" type="radio" name="available" value="true" checked>
-                              <label class="form-check-label">Si</label>
-                              @error('available')
-                              <small class="text-danger">{{ $message }}</small>
-                              @enderror
-                          </div>
-                          <div class="form-check form-check-inline">
-                              <input class="form-check-input form-control @error('available') is-invalid @enderror" type="radio" name="available" value="false">
-                              <label class="form-check-label">No</label>
-                              @error('available')
-                              <small class="text-danger">{{ $message }}</small>
-                              @enderror
-                          </div> --}}
-                          <!-- multiselezione categorie -->
-
-                          <div class="form-group row mb-0">
-                              <div class="col-md-6 offset-md-4">
-                                  <button type="submit" class="my button my-button-orange">
-                                      {{ __('Aggiungi questo Ristorante') }}
-                                  </button>
-                              </div>
-                          </div>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+                        <div class="form-group column mb-0">
+                            <div class="col-md-6">
+                                <button type="submit" class="my button my-button-orange">
+                                    {{ __('Aggiungi questo Ristorante') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+</div>
 </section>
 @endsection
--- resources/views/guests/restaurants/index.blade.php
+++ resources/views/guests/restaurants/index.blade.php
@@ -2,37 +2,35 @@
 
 @section('main')
 <main id="root">
-<div class="container-fluid banner-show" style="background-image: url('../images/varie/my-restaurant-banner.jpg')"></div>
-<section class="section-main">
-  <div class="container search-bar">
-    <label for="Ricerca Ristoranti"></label>
-    <input v-model="scriviTxt" class="search" type="search" name="search" @keyup.enter="cerca(scriviTxt)" placeholder="Inserisci il nome del ristorante">
-    <a class="my-button my-button-orange" style="cursor: pointer; margin-left: 15px;" @click="cerca(scriviTxt)" >
-      Cerca
-    </a>
-  </div>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-2 card-homepage"
-                    v-for="category in categories"
-                    :key="category.id"
-                    @onClick="categoryFilter()">
-                        <h4>@{{category.name}}</h4>
-                        <img class="icon_category" :src="category.icon">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <hr v-if="searchResult.length > 0">
-        <h2 v-if="searchResult.length > 0">Risultati della ricerca</h2>
+    <div class="container-fluid banner-show" style="background-image: url('../images/varie/my-restaurant-banner.jpg')"></div>
+    <section class="section-main">
+    <div class="container search-bar">
+        <label for="Ricerca Ristoranti"></label>
+        <input v-model="scriviTxt" class="search" type="search" name="search" @keyup.enter="cerca(scriviTxt)" placeholder="Inserisci il nome del ristorante">
+        <a class="my-button my-button-orange" style="cursor: pointer; margin-left: 15px;" @click="cerca(scriviTxt)" >
+        Cerca
+        </a>
+    </div>
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-12">
-
                     <div class="row">
-                        <div class="col-md-4 col-lg-4 mt-2 card-outline" v-for="result in searchResult" :key="result.id ">
+                        <div class="col-md-2 card-homepage"
+                        v-for="category in categories"
+                        :key="category.id"
+                        @onClick="categoryFilter()">
+                            <h4>@{{category.name}}</h4>
+                            <img class="icon_category" :src="category.icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2 v-if="searchResult.length > 0">Risultati della ricerca</h2>
+            <hr v-if="searchResult.length > 0">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-4 col-lg-4 mt-2 card-outline" v-for="result in searchResult" :key="result.id">
                             <a class="link-to" :href="'restaurants/' + result.slug">
                                 <div class="card-personal">
                                     <div class="card-personal-cover" :style="`background-image: url('${result.banner}') ; `">
@@ -46,38 +44,31 @@
                             </a>
                         </div>
                     </div>
-
-              </div>
-          </div>
-      </div>
-
-
-
-      <hr>
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-12">
-
-                  <div class="row">
-                      <div class="col-md-4 col-lg-4 mt-2 card-outline" v-for="restaurant in restaurants" :key="restaurants.id ">
-                          <a class="link-to" :href="'restaurants/' + restaurant.slug">
-                              <div class="card-personal">
-                                  <div class="card-personal-cover" :style="`background-image: url('${restaurant.banner}') ; `">
-                                      <img :src="restaurant.logo">
-                                      <div class="overlay"></div>
-                                  </div>
-                                  <div class="card-personal-title">
-                                      <h4>@{{restaurant.name}}</h4>
-                                  </div>
-                              </div>
-                          </a>
-                      </div>
-                  </div>
-
-              </div>
-          </div>
-      </div>
-
-</section>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-4 col-lg-4 mt-2 card-outline" v-for="restaurant in restaurants" :key="restaurants.id ">
+                            <a class="link-to" :href="'restaurants/' + restaurant.slug">
+                                <div class="card-personal">
+                                    <div class="card-personal-cover" :style="`background-image: url('${restaurant.banner}') ; `">
+                                        <img :src="restaurant.logo">
+                                        <div class="overlay"></div>
+                                    </div>
+                                    <div class="card-personal-title">
+                                        <h4>@{{restaurant.name}}</h4>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 @endsection
