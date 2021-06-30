be49fbd3edc11574e35af90def411838e6ccee4b
fabio <parisifabio98@gmail.com>
2021-06-30 18:11:00 +0200

    creato api post che prendono i dish

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Order;
+use App\Dish;
 use Braintree\Gateway;
+use App\Http\Requests\OrderRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,16 +15,37 @@ class OrderController extends Controller
 
   public function generate(Request $request, Gateway $gateway)
   {
-    $token =$gateway->clientToken()->generate();
+    $token = $gateway->clientToken()->generate();
     $data = [
       'success' => true,
       'token' => $token,
     ];
-    return response() -> json($data,200);
+    return response()->json($data, 200);
   }
 
-  public function makePayment(Request $request, Gateway $gateway)
+  public function makePayment(OrderRequest $request, Gateway $gateway)
   {
-    return 'make payment';
+    $dish = Dish::find($request->dish);
+    $result = $gateway->transaction()->sale([
+      'amount' => $dish->price,
+      'paymentMethodNonce' => $request->token,
+      'options' => [
+        'submitForSettlement' => true,
+      ],
+    ]);
+
+    if ($result->success) {
+      $data = [
+        'success' => true,
+        'message' => 'transazione eseguita con successo',
+      ];
+      return response()->json($data, 200);
+    } else {
+      $data = [
+        'success' => false,
+        'message' => 'transazione fallita',
+      ];
+      return response()->json($data, 401);
+    }
   }
 }
