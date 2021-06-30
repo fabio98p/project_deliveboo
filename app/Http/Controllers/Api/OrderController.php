771310f4500265ab49b9449c42f77f513358599d
fabio <parisifabio98@gmail.com>
2021-06-30 16:43:41 +0200

    inizio braintree con chiamate api per i token

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Order;
+use Braintree\Gateway;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class OrderController extends Controller
+{
+
+  public function generate(Request $request, Gateway $gateway)
+  {
+    $token =$gateway->clientToken()->generate();
+    $data = [
+      'success' => true,
+      'token' => $token,
+    ];
+    return response() -> json($data,200);
+  }
+
+  public function makePayment(Request $request, Gateway $gateway)
+  {
+    return 'make payment';
+  }
+}
