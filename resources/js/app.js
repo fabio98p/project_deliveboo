e0cbb5dd5a74583c10c3e8c86b45134a341d46f8
Giorgio <giorgio.pignattari@gmail.it>
2021-07-02 10:26:54 +0200

    inizio braintree config

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ window.Vue = require('vue');
 Vue.component('dishes', require('./components/Dishes.vue').default);
 Vue.component('cart', require('./components/Cart.vue').default);
 Vue.component('cart-checkout', require('./components/CartCheckout.vue').default);
-Vue.component('payment', require('./components/Payment.vue').default);
 
 import store from './store.js';
 
