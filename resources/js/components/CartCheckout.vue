66b4ccbd61ffba18e35a97162667b3a91fd3db9d
emanuele <emattina97@gmail.com>
2021-07-03 17:23:44 +0200

    Vista checkoutbraintree funzionante

--- resources/js/components/CartCheckout.vue
+++ resources/js/components/CartCheckout.vue
@@ -1,85 +1,87 @@
 <template>
-	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-4" id="cart">
-		<div class="checkout-card" :class="showCart == true ? 'd-block' : ''">
-			<div class="page-top">
-				<h2>Riepilogo ordine</h2>
-				<div class="my-buttons-container">
-					<button
-						class="my-button my-button-orange"
-						:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
-						@click="clickEditCart = !clickEditCart"
+	<div
+		id="cart"
+		class="checkout-card"
+		:class="showCart == true ? 'd-block' : ''"
+	>
+		<div class="page-top">
+			<h2>Riepilogo ordine</h2>
+			<div class="my-buttons-container">
+				<button
+					class="my-button my-button-orange"
+					:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
+					@click="clickEditCart = !clickEditCart"
+				>
+					<i class="fas fa-edit"></i>
+				</button>
+			</div>
+		</div>
+		<div class="cart-inner">
+			<div
+				class="cart-empty text-center"
+				:class="$store.state.cartCount == 0 ? 'd-block' : 'd-none'"
+			>
+				<h5>Il tuo carello è vuoto!</h5>
+			</div>
+			<div class="cart-item" v-for="item in $store.state.cart" :key="item.id">
+				<div class="dish-cover">
+					<!-- <img :src="item.image" :alt="item.name" /> -->
+
+					<div
+						class="dish-cover-image"
+						:style="`background-image: url('${item.image}');`"
 					>
-						<i class="fas fa-edit"></i>
-					</button>
+						<div class="dish-cover-overlay" v-if="clickEditCart">
+							<i class="fas fa-times" @click="removeItemFromCart(item)"></i>
+						</div>
+					</div>
+					<div class="dish-name ml-3">
+						<span>{{ item.name }}</span>
+						<div class="dish-quantity">
+							<i class="fas fa-minus" @click="removeFromCart(item)"></i>
+							<span
+								@click="setManually(item.id)"
+								v-if="item.id != currentId"
+								class="ml-1 mr-1 cursor-pointer"
+								>{{ item.quantity }}</span
+							>
+							<input
+								v-if="item.id == currentId"
+								autofocus
+								@focus="$event.target.select()"
+								class="set-quantity"
+								type="number"
+								:value="item.quantity"
+								v-on:keyup.enter="changeQuantity(item, $event)"
+							/>
+							<i class="fas fa-plus" @click="addToCart(item)"></i>
+						</div>
+					</div>
+				</div>
+				<div class="dish-price">
+					<span>€{{ item.totalPrice }}</span>
 				</div>
 			</div>
-			<div class="cart-inner">
-				<div
-					class="cart-empty text-center"
-					:class="$store.state.cartCount == 0 ? 'd-block' : 'd-none'"
-				>
-					<h5>Il tuo carello è vuoto!</h5>
-				</div>
-				<div class="cart-item" v-for="item in $store.state.cart" :key="item.id">
-					<div class="dish-cover">
-						<!-- <img :src="item.image" :alt="item.name" /> -->
 
-						<div
-							class="dish-cover-image"
-							:style="`background-image: url('${item.image}');`"
-						>
-							<div class="dish-cover-overlay" v-if="clickEditCart">
-								<i class="fas fa-times" @click="removeItemFromCart(item)"></i>
-							</div>
-						</div>
-						<div class="dish-name ml-3">
-							<span>{{ item.name }}</span>
-							<div class="dish-quantity">
-								<i class="fas fa-minus" @click="removeFromCart(item)"></i>
-								<span
-									@click="setManually(item.id)"
-									v-if="item.id != currentId"
-									class="ml-1 mr-1 cursor-pointer"
-									>{{ item.quantity }}</span
-								>
-								<input
-									v-if="item.id == currentId"
-									autofocus
-									@focus="$event.target.select()"
-									class="set-quantity"
-									type="number"
-									:value="item.quantity"
-									v-on:keyup.enter="changeQuantity(item, $event)"
-								/>
-								<i class="fas fa-plus" @click="addToCart(item)"></i>
-							</div>
-						</div>
-					</div>
-					<div class="dish-price">
-						<span>€{{ item.totalPrice }}</span>
-					</div>
-				</div>
-
-				<div
-					class="cart-sum mt-3"
-					:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
+			<div
+				class="cart-sum"
+				:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
+			>
+				<span>Prodotti nel carello: {{ $store.state.cartCount }}</span>
+				<h4 style="font-weight: 700">Totale: €{{ totalPrice }}</h4>
+			</div>
+			<div
+				class="text-center"
+				:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
+			>
+				<a href="">
+					<button class="my-button my-button-orange">
+						Torna al ristorante
+					</button></a
 				>
-					<span>Prodotti nel carello: {{ $store.state.cartCount }}</span>
-					<p>Totale: €{{ totalPrice }}</p>
-				</div>
-				<div
-					class="mt-4 text-center"
-					:class="$store.state.cartCount != 0 ? 'd-block' : 'd-none'"
-				>
-					<a href="">
-						<button class="my-button my-button-orange">
-							Torna al ristorante
-						</button></a
-					>
-					<button class="mybutton my-button-red mt-3" @click="emptyCart">
-						Svuota il carello</button
-					><br />
-				</div>
+				<button class="mybutton my-button-red mt-3" @click="emptyCart">
+					Svuota il carello</button
+				><br />
 			</div>
 		</div>
 	</div>
