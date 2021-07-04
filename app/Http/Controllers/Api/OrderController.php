d8630c21b10131a7df337a9d5997622c88d8d912
fabio <parisifabio98@gmail.com>
2021-07-04 15:31:25 +0200

    creazione del api che rimanda il totale dei guadagni del ristorante per mese

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -15,11 +15,51 @@ class OrderController extends Controller
 
   public function ordersShow($restaurant)
   {
-    $orders = Order::where('restaurant_id',$restaurant)->orderBy('created_at','desc')->get();
+    $orders = Order::where('restaurant_id', $restaurant)->orderBy('created_at', 'desc')->get();
 
     return response()->json([
-        'response' => $orders,
-        'success' => true,
+      'response' => $orders,
+      'success' => true,
+    ]);
+  }
+
+  public function totalForMonth($restaurant)
+  {
+    $orders = Order::where('restaurant_id', $restaurant)->get();
+    $totalForMonth = [
+      "01" => 0,
+      "02" => 0,
+      "03" => 0,
+      "04" => 0,
+      "05" => 0,
+      "06" => 0,
+      "07" => 0,
+      "08" => 0,
+      "09" => 0,
+      "10" => 0,
+      "11" => 0,
+      "12" => 0,
+    ];
+    $price = 0;
+    foreach ($orders as $order) {
+      $totalForPrice[] = $order->total_price;
+      $month = date("m", strtotime($order->created_at));
+      $year = date("Y", strtotime($order->created_at));
+
+      if (date("Y") == $year) {
+        foreach ($totalForMonth as $key => $element) {
+          if ($month == (int)$key) {
+            $totalForMonth[$key] = $totalForMonth[$key] + $order->total_price;
+          }
+        }
+      }
+    }
+
+    return response()->json([
+      'response' => [
+        $totalForMonth,
+      ],
+      'success' => true,
     ]);
   }
 }
